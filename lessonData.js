8bd444e09638fb308c92284d1b19dffa6fa77aea
SSuraj1409 <ssuraj1409@gmail.com>
2025-10-26 15:19:29 +0400

    update13

---
+++ lessonData.js
@@ -0,0 +1,110 @@
+const lessonData = [
+  {
+    id: 1,
+    subject: 'Robotics Club',
+    location: 'UK',
+    price: 15,
+    spaces: 5,
+    rating: 4,
+    image: 'images/robotics.png'
+  },
+  {
+    id: 2,
+    subject: 'Public Speaking',
+    location: 'USA',
+    price: 12,
+    spaces: 5,
+    rating: 5,
+    image: 'images/public speaking.png'
+  },
+  {
+    id: 3,
+    subject: 'Art Workshop',
+    location: 'France',
+    price: 10,
+    spaces: 5,
+    rating: 3,
+    image: 'images/art.png'
+  },
+  {
+    id: 4,
+    subject: 'Astronomy Club',
+    location: 'Germany',
+    price: 18,
+    spaces: 5,
+    rating: 5,
+    image: 'images/astronomy.png'
+  },
+  {
+    id: 5,
+    subject: 'Coding Bootcamp',
+    location: 'India',
+    price: 20,
+    spaces: 5,
+    rating: 4,
+    image: 'images/coding.png'
+  },
+  {
+    id: 6,
+    subject: 'Music Lessons',
+    location: 'Italy',
+    price: 14,
+    spaces: 5,
+    rating: 3,
+    image: 'images/music.png'
+  },
+  {
+    id: 7,
+    subject: 'Drama Club',
+    location: 'Australia',
+    price: 13,
+    spaces: 5,
+    rating: 4,
+    image: 'images/drama.png'
+  },
+  {
+    id: 8,
+    subject: 'Football Training',
+    location: 'Brazil',
+    price: 16,
+    spaces: 5,
+    rating: 5,
+    image: 'images/football.png'
+  },
+  {
+    id: 9,
+    subject: 'Gardening Club',
+    location: 'Japan',
+    price: 15,
+    spaces: 5,
+    rating: 4,
+    image: 'images/gardening.png'
+  },
+  {
+    id: 10,
+    subject: 'Photography Club',
+    location: 'Canada',
+    price: 11,
+    spaces: 5,
+    rating: 4,
+    image: 'images/photography.png'
+  },
+  {
+    id: 11,
+    subject: 'Chess Club',
+    location: 'Russia',
+    price: 8,
+    spaces: 5,
+    rating: 3,
+    image: 'images/chess.png'
+  },
+  {
+    id: 12,
+    subject: 'Creative Writing',
+    location: 'Spain',
+    price: 9,
+    spaces: 5,
+    rating: 5,
+    image: 'images/creative writing.png'
+  }
+];
